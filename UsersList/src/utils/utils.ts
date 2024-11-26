a03073e8452c020a940ce3fbd24a91f79d4fe01f
reuven12 <86264874+reuven12@users.noreply.github.com>
2024-11-26 11:40:07 +0200

    Update utils.ts

--- UsersList/src/utils/utils.ts
+++ UsersList/src/utils/utils.ts
@@ -42,57 +42,12 @@ export const generateUser = (user: User): UserEntity => {
   return userEntity;
 };
 
-import { UserModel } from "../models/UserModel";
-
-export class MongoUserRepository {
-  async findByUniqueKey(uniqueKey: string): Promise<any | null> {
-    return await UserModel.findOne({ uniqueKey }).lean().exec(); // שימוש ב-lean()
-  }
-
-  async updateByUniqueKey(uniqueKey: string, updatedFields: any): Promise<any> {
-    return await UserModel.updateOne({ uniqueKey }, { $set: updatedFields }).exec();
-  }
-
-  async createUser(data: any): Promise<any> {
-    const user = new UserModel(data);
-    return await user.save();
-  }
-}
-
-import { IUserRepository } from "../repositories/IUserRepository";
-
-export class UserService {
-  constructor(private userRepository: IUserRepository) {}
-
-  async createOrUpdateUser(data: any): Promise<any> {
-    const { uniqueKey } = data;
-
-    // חיפוש משתמש קיים
-    const existingUser = await this.userRepository.findByUniqueKey(uniqueKey);
-
-    if (existingUser) {
-      // המשתמש קיים - בדיקת השדות שהשתנו
-      const updatedFields: Record<string, any> = {};
-      for (const key in data) {
-        if (data[key] !== existingUser[key]) {
-          updatedFields[key] = data[key];
-        }
-      }
-
-      // אם יש שדות לעדכון, מבצעים עדכון
-      if (Object.keys(updatedFields).length > 0) {
-        await this.userRepository.updateByUniqueKey(uniqueKey, updatedFields);
-      }
-
-      // מחזירים את האובייקט המשולב
-      return { ...existingUser, ...updatedFields };
-    } else {
-      // המשתמש לא קיים - יצירה
-      return await this.userRepository.createUser(data);
+for (const key in data) {
+  if (Object.hasOwn(data, key)) { // וידוא שהשדה קיים ב-data
+    if (data[key] !== existingUser[key]) {
+      updatedFields[key] = data[key];
     }
   }
 }
-
-
 
 
